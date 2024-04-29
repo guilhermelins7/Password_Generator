b1ac3aaed4683a6970c99413bf4e73aff5bf8056
Guilherme Lins <guilherme7lins@gmail.com>
2024-04-29 00:14:36 -0300

    Password_Generator1

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Json;
+
+namespace Password_Generation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int passwordSize;
+            string password = "";
+            string charList = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz@#%&!";
+
+            Console.WriteLine("Informe o tamanho da senha que deve ser gerada:");
+            passwordSize = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < passwordSize; i++)
+            {
+                int index = random.Next(charList.Length);
+                password += charList[index];
+            }
+
+            Console.WriteLine(password);
+        }
+    }
+}
